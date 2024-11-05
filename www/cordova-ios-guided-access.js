d595d0759bb68e59ec293f6337ef09c5013c9cac
Nicolas Leclerc <nicolas@coqsenpate.com>
2024-11-05 16:30:51 +0100

    Changed api to use promises instead of callbacks.

--- www/cordova-ios-guided-access.js
+++ www/cordova-ios-guided-access.js
@@ -1,15 +1,25 @@
-var exec = require('cordova/exec');
+var exec = require('cordova/exec')
+
+function processErr(reject) {
+	return (errorMessage)=> reject(new Error(errorMessage))
+}
 
 var WPGuidedAccess = {
-    start: function(success, error) {
-        exec(success, error, 'WPGuidedAccessMode', 'start', []);
-    },
-    end: function(success, error) {
-        exec(success, error, 'WPGuidedAccessMode', 'end', []);
-    },
-    isStarted: function(success, error) {
-        exec(success, error, 'WPGuidedAccessMode', 'isEnabled', []);
-    }
+	start: function(success, error) {
+		return new Promise((resolve,reject)=> {
+			exec(resolve, processErr(reject), 'WPGuidedAccessMode', 'start', []);
+		})
+	},
+	end: function(success, error) {
+		return new Promise((resolve,reject)=> {
+			exec(resolve, processErr(reject), 'WPGuidedAccessMode', 'end', []);
+		})
+	},
+	isStarted: function(success, error) {
+		return new Promise((resolve,reject)=> {
+			exec(resolve, processErr(reject), 'WPGuidedAccessMode', 'isEnabled', []);
+		})
+	}
 };
 
 module.exports = WPGuidedAccess;
